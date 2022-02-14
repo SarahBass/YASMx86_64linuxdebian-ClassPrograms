2675e1aae1918f232462c4b0100801547c0480ac
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-02-14 13:46:07 -0800

    Update twohello.s

--- twohello.s
+++ twohello.s
@@ -1,86 +1,35 @@
 ;;-----------------------------------------------------------------------------.
  ; Student: Sarah Bass                                                         |
  ; Program: hello.s                                                            |
- ; Description: Prints "Hello, world!" in multiple lines and an text art hello |
+ ; Description: Prints "Hello, world!" in multiple lines using different loops |
 ;;-----------------------------------------------------------------------------'
 
 section .data
-
-msg:            db      "Hello, world!", 10
+;Loop type Manual
+msg:            db      "Hello, world!", 10 , "Hallo, welt!", 10 ,  "Hola, Mundo!", 10, "Hej världen!", 10
 MSGLEN:         equ     $-msg
 
-msg2:            db      "Hallo, welt!", 10
-MSGLEN2:         equ     $-msg2
+earth:          db      " _          _ _                            _     _ ", 10, "| |__   ___| | | ___   __      _____  _ __| | __| |", 10 , "| '_ \ / _ \ | |/ _ \  \ \ /\ / / _ \| '__| |/ _` |", 10 , "| | | |  __/ | | (_) |  \ V  V / (_) | |  | | (_| |", 10 , "|_| |_|\___|_|_|\___/    \_/\_/ \___/|_|  |_|\__,_|", 10                      
+EARTHLEN:       equ     $-earth
 
-msg3:            db      "Hola, Mundo!", 10
-MSGLEN3:         equ     $-msg3
-
-msg4:            db      "Hej världen!", 10
-MSGLEN4:         equ     $-msg4
-
-earth:            db      " _          _ _                            _     _ ", 10
-EARTHLEN:         equ     $-earth
-
-earth1:            db      "| |__   ___| | | ___   __      _____  _ __| | __| |", 10
-EARTHLEN1:         equ     $-earth1
-
-earth2:            db      "| '_ \ / _ \ | |/ _ \  \ \ /\ / / _ \| '__| |/ _` |", 10
-EARTHLEN2:         equ     $-earth2
-
-earth3:            db      "| | | |  __/ | | (_) |  \ V  V / (_) | |  | | (_| |", 10
-EARTHLEN3:         equ     $-earth3
-
-earth4:            db      "|_| |_|\___|_|_|\___/    \_/\_/ \___/|_|  |_|\__,_|", 10
-EARTHLEN4:         equ     $-earth4
-
-
-
-
-
-
+;Loop type do while 
+looptext :      db      "🌎", 3
+loopLEN:        equ     $-looptext
 
 section .text
-
-;; Program code goes here
 
 global _start
 _start:
     ;call _msg
-    ;call _msg2
-    ;call _msg3
-    ;call _msg4
-
     ;call _earth
-    ;call _earth2
-    ;call _earth3
-    ;call _earth4
+    
+    
     syscall
-_msg:
+    _msg:
     mov     rax,    1               ; Syscall code in rax
     mov     rdi,    1               ; 1st arg, file desc. to write to
     mov     rsi,    msg             ; 2nd arg, addr. of message
     mov     rdx,    MSGLEN          ; 3rd arg, num. of chars to print
-    syscall
-
-    _msg2:
-    mov     rax,    1                ; Syscall code in rax
-    mov     rdi,    1                ; 1st arg, file desc. to write to
-    mov     rsi,    msg2             ; 2nd arg, addr. of message
-    mov     rdx,    MSGLEN2          ; 3rd arg, num. of chars to print
-    syscall
-
-    _msg3:
-    mov     rax,    1                ; Syscall code in rax
-    mov     rdi,    1                ; 1st arg, file desc. to write to
-    mov     rsi,    msg3             ; 2nd arg, addr. of message
-    mov     rdx,    MSGLEN3          ; 3rd arg, num. of chars to print
-    syscall
-
-     _msg4:
-    mov     rax,    1                ; Syscall code in rax
-    mov     rdi,    1                ; 1st arg, file desc. to write to
-    mov     rsi,    msg4             ; 2nd arg, addr. of message
-    mov     rdx,    MSGLEN4          ; 3rd arg, num. of chars to print
     syscall
 
     _earth:
@@ -90,37 +39,20 @@ _msg:
     mov     rdx,    EARTHLEN          ; 3rd arg, num. of chars to print
     syscall
 
-     _earth1:
-    mov     rax,    1               ; Syscall code in rax
-    mov     rdi,    1               ; 1st arg, file desc. to write to
-    mov     rsi,    earth1          ; 2nd arg, addr. of message
-    mov     rdx,    EARTHLEN1       ; 3rd arg, num. of chars to print
+    mov     rcx,    26
+
+start_loop:
+
+    mov     rax,    1                 ; Syscall code in rax
+    mov     rdi,    1                 ; 1st arg, file desc. to write to
+    mov     rsi,    looptext          ; 2nd arg, addr. of message
+    mov     rdx,     loopLEN          ; 3rd arg, num. of chars to print
+    
+    mov     r15,     rcx
     syscall
-
-     _earth2:
-    mov     rax,    1               ; Syscall code in rax
-    mov     rdi,    1               ; 1st arg, file desc. to write to
-    mov     rsi,    earth2          ; 2nd arg, addr. of message
-    mov     rdx,    EARTHLEN2       ; 3rd arg, num. of chars to print
-    syscall
-
-     _earth3:
-    mov     rax,    1               ; Syscall code in rax
-    mov     rdi,    1               ; 1st arg, file desc. to write to
-    mov     rsi,    earth3          ; 2nd arg, addr. of message
-    mov     rdx,    EARTHLEN3       ; 3rd arg, num. of chars to print
-    syscall
-
-    _earth4:
-    mov     rax,    1               ; Syscall code in rax
-    mov     rdi,    1               ; 1st arg, file desc. to write to
-    mov     rsi,    earth4          ; 2nd arg, addr. of message
-    mov     rdx,    EARTHLEN4       ; 3rd arg, num. of chars to print
-    syscall
-
-
-
-     
+    mov      rcx,    r15
+    loop start_loop
+  
     ;; Terminate process
     mov     rax,    60              ; Syscall code in rax
     mov     rdi,    0               ; First parameter in rdi
